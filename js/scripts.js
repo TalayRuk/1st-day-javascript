d03109c9aa8dc84d2637bdaa8db684b20135721f
vp <notejde@gmail.com>
2016-08-10 16:46:09 -0700

    added calcultors btns w/functionss

--- js/scripts.js
+++ js/scripts.js
@@ -21,16 +21,6 @@
 
 //Everything below this line is user interface logic:
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-  event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-
-  var result = add(number1, number2);
-  $("#output").text(result);
-
-  });
-
   $("#clickme").click(function() {
     $("#cal").animate({
       opacity: 0.25,
@@ -41,5 +31,40 @@ $(document).ready(function() {
     });
   });
 
+  $("#addbtn").click(function(event) {
+  event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+
+  var result = add(number1, number2);
+  $("#output").text(result);
+
+  });
+  $("#subtbtn").click(function(event) {
+  event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+
+  var result = subtract(number1, number2);
+  $("#output").text(result);
+
+  });
+
+  $("#multiplybtn").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("#dividebtn").click(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
 
 });
